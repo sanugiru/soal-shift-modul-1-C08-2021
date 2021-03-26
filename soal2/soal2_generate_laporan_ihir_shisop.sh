bc84feb4c48cade6231d45d18619758e9ab10ca2
Sena <72660860+sanugiru@users.noreply.github.com>
2021-03-26 23:15:52 +0700

    Add files via upload

---
+++ soal2/soal2_generate_laporan_ihir_shisop.sh
@@ -0,0 +1,54 @@
+#!bin/bash
+
+#Nomor-2a
+awk -F '\t' '
+{	
+
+	if (NR>1) profitPercentage[$1] = $21 / ($18 - $21) * 100
+	
+	for (ID in profitPercentage){
+		if(max <= profitPercentage[ID] ){
+			max = profitPercentage[ID]
+			rowID = ID
+		}	
+	}	
+} END {print "Transaksi terakhir dengan percentage profit terbesar yaitu", rowID,"dengan persentase", max,"%\n"}' Laporan-TokoShiSop.tsv > hasil.txt
+
+
+#Nomor-2b
+echo "Daftar nama customer di Albuquerque pada tahun 2017 antara lain:" >> hasil.txt
+
+awk -F '\t' '$10 ~ /Albuquerque/ && $2 ~ /2017/ {print $7}' Laporan-TokoShiSop.tsv >> hasil.txt
+
+
+#Nomor-2c
+awk -F "\t" '
+{	
+	min = segCust[$8]
+	segCust[$8]++
+
+	for (seg in segCust){
+		if(min>segCust[seg] && seg!="Segment")
+		{
+			min = segCust[seg]
+			segment = seg
+		}
+	}
+} END {print "\nTipe segmen customer yang penjualannya paling sedikit adalah", segment, "dengan", min, "transaksi."}' Laporan-TokoShiSop.tsv >> hasil.txt
+
+
+#Nomor-2d
+awk -F '\t' '
+{	
+	min = profit[$13]	
+	profit[$13] += $21
+
+	for(reg in profit)
+	{
+		if(min>profit[reg] && reg!="Region")
+		{
+			min = profit[reg]
+			region = reg
+		}
+	}
+} END {print "\nWilayah bagian (region) yang memiliki keuntungan (profit) paling sedikit adalah", region,"dengan total keuntungan",min}' Laporan-TokoShiSop.tsv >> hasil.txt
