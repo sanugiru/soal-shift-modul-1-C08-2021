7f6fe0b97bea140196e2e578d3116c94326cf537
Deka Julian Arrizki <55046884+Arrizki16@users.noreply.github.com>
2021-04-01 14:15:09 +0700

    Update soal3a.sh

--- soal3/soal3a.sh
+++ soal3/soal3a.sh
@@ -2,24 +2,41 @@
 
 #dijalankan di dir hasil3a
 
-exec &> Foto.log
-for i in {1..23}
+# !/bin/bash
+
+#dijalankan di dir hasil3a
+len=22
+i=0
+while [ "$i" -le "$len" ]
 do
-	wget "https://loremflickr.com/320/240/kitten"
+	if [[ $i -eq 0 ]]; then
+		wget -O "kitten" -a Foto.log https://loremflickr.com/320/240/kitten
+	else
+		wget -O "kitten.$i" -a Foto.log https://loremflickr.com/320/240/kitten
+	fi
+	
+	flag=0
+    check=($(awk '/https:\/\/loremflickr.com\/cache\/resized\// {print $3}' ./Foto.log))
+    length=(${#check[@]})
+    for((j=0; j < ($length-1); j++))
+    do
+        if [ "${check[j]}" == "${check[$(($length-1))]}" ]
+        then
+            flag=1
+            break
+        fi
+    done
+    if [ $flag -eq 1 ]
+    then
+        len=$(($len - 1))
+        if [[ $i -eq 0 ]]; then
+        	rm "kitten"
+        fi
+        rm "kitten.$i"
+    else
+        i=$(($i + 1)) 
+    fi
 done
-
-declare -A arr
-shopt -s globstar
-
-for file in **; do
-  [[ -f "$file" ]] || continue
-   
-  read cksm _ < <(md5sum "$file")
-  if ((arr[$cksm]++)); then 
-    rm "$file"
-  fi
-done
-
 
 flag=1
 for X in kitten*; do
