9889d2afb53b3d641d1fcd63bc499d9cf76ffc72
Muhammad Farhan Haykal <70801807+neroSpace@users.noreply.github.com>
2021-04-02 19:03:03 +0700

    d dan e

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -12,6 +12,25 @@ grep INFO syslog.log | rev | cut -d"(" -f1 | rev|  cut -d")" -f1 | sort | uniq -
 
 #d
 echo "Error,Count" > error_message.csv
+jenis=`grep ERROR syslog.log | cut -d ":" -f4 | cut -d"(" -f1 | cut -d" " -f3-8 | sort | uniq`
+reg=`grep ERROR syslog.log | cut -d ":" -f4 | cut -d"(" -f1 | cut -d" " -f3-8`
+while read x
+do
+	counter=`echo "$reg"|sort | grep -c "$x"`
+	hasil=`printf "$hasil\n$x,$counter"`
+done<<<`printf "$jenis"`
+hasil=`echo "$hasil" | sort -rnk2t',' `
+printf "$hasil" >> error_message.csv
 
 #e
 echo "Username,INFO,ERROR" > user_statistic.csv
+nama=`cut -d":" -f 4 syslog.log | rev | cut -d"(" -f1 | rev | cut -d")" -f1| sort | uniq`
+reg=`cut -d":" -f 4 syslog.log | rev | cut -d"(" -f1| rev | cut -d ")" -f1|sort`
+while read y
+do
+	error=`cut -d":" -f4 syslog.log | grep "$y" | grep -c "ERROR"`
+	info=`cut -d":" -f4 syslog.log | grep "$y" | grep -c "INFO"`
+	stat=`printf "$y,$info,$error\n"`
+	hasilstat=`printf "$hasilstat\n$stat"`
+done <<< `printf "$nama"`
+printf "$hasilstat" >> user_statistic.csv
